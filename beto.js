76d6f5e7ac24d14b11ea38872aaaa28520085585
Carlos Roberto Giancristoforo <betorgx@hotmail.com>
2018-11-04 18:04:05 -0200

    Atualizações...

--- beto.js
+++ beto.js
@@ -3,5 +3,67 @@ $(document).ready(function () {
         "scrollY":        "350px",
         "scrollCollapse": true,
         "paging":         true
-    } );
+    });
+	
+	setMasks();
+	
+	jQuery(function($){
+	$.datepicker.regional['pt-BR'] = {
+		closeText: 'Fechar',
+		prevText: '<Anterior',
+		nextText: 'Próximo>',
+		currentText: 'Hoje',
+		monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho',
+		'Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+		monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun',
+		'Jul','Ago','Set','Out','Nov','Dez'],
+		dayNames: ['Domingo','Segunda-feira','Terça-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sabado'],
+		dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+		dayNamesMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+		weekHeader: 'Sm',
+		dateFormat: 'dd/mm/yy',
+		firstDay: 0,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
+		$.datepicker.setDefaults($.datepicker.regional['pt-BR']);
+	});
+  $( function() {
+    $( "#datepicker" ).datepicker($.datepicker.regional['pt-BR']);
+	});
 });
+function setMasks(){
+	$('.date').mask('00/00/0000');
+	$('.time').mask('00:00:00');
+	$('.date_time').mask('00/00/0000 00:00:00');
+	$('.cep').mask('00000-000');
+	$('.phone').mask('0000-0000');
+	$('.phone_with_ddd').mask('(00) 0000-0000');
+	$('.phone_us').mask('(000) 000-0000');
+	$('.mixed').mask('AAA 000-S0S');
+	$('.cpf').mask('000.000.000-00', {reverse: true});
+	$('.cnpj').mask('00.000.000/0000-00', {reverse: true});
+	$('.money').mask('000.000.000.000.000,00', {reverse: true});
+	$('.money2').mask("#.##0,00", {reverse: true});
+	$('.ip_address').mask('0ZZ.0ZZ.0ZZ.0ZZ', {
+	translation: {
+	  'Z': {
+		pattern: /[0-9]/, optional: true
+	  }
+	}
+	});
+	$('.ip_address').mask('099.099.099.099');
+	$('.percent').mask('##0,00%', {reverse: true});
+	$('.clear-if-not-match').mask("00/00/0000", {clearIfNotMatch: true});
+	$('.placeholder').mask("00/00/0000", {placeholder: "__/__/____"});
+	$('.fallback').mask("00r00r0000", {
+	  translation: {
+		'r': {
+		  pattern: /[\/]/,
+		  fallback: '/'
+		},
+		placeholder: "__/__/____"
+	  }
+	});
+	$('.selectonfocus').mask("00/00/0000", {selectOnFocus: true});
+};
